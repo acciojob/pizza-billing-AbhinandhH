cb9c0294c2f81cc96f12f3c23ce95bd8dc7bc59b
Abhinandh Haridas <abhinandhabhi53@gmail.com>
2023-01-17 16:57:13 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,6 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza p = new DeluxePizza(false);
-    p.addTakeaway();
-    p.addExtraCheese();
-    p.addExtraCheese();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+
   }
 }
