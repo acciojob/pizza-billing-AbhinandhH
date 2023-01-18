2b904babc28848e2e4396be47550319f6ea495ff
Abhinandh Haridas <abhinandhabhi53@gmail.com>
2023-01-19 01:14:23 +0530

    falls commit1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -55,15 +55,12 @@ public class Pizza {
     }
 
     public String getBill(){
-        this.bill += "Base Price Of The Pizza: " + this.basePizzaPrice + "\n";
-        if(this.isCheese) this.bill += "Extra Cheese Added: " + this.cheesePrice + "\n";
-        else this.bill += "Extra Cheese Added: " +  "\n";
-        if(isToppings) this.bill += "Extra Toppings Added: " + this.toppingPrice + "\n";
-        else this.bill += "Extra Toppings Added: " +  "\n";
-        if(isPaperbag) this.bill += "Paperbag Added: " + this.bagPrice + "\n";
-        else this.bill += "Paperbag Added: " + "\n";
-        this.bill += "Total Price: " + this.getPrice();
-
+        this.bill = ("Base Price Of The Pizza: " + basePizzaPrice+
+                "\nExtra Cheese Added: 80"  +
+                "\nExtra Toppings Added: "  + (isVeg?70:120) +
+                (isPaperbag?"\nPaperbag Added: 20":"") +
+                "\nTotal Price: "+ this.price + "\n"
+        );
         return this.bill;
     }
 }
